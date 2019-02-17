802c11d3b1c0277519566ea549a55a10f679a9b1
Ján Hrnko <jan.hrnko@nexthop.sk>
2019-02-17 17:53:04 +0100

    Moar tests and string generator

---
+++ generator.sh
@@ -0,0 +1,8 @@
+< /dev/urandom tr -dc A-Z | head -c${1:-50};echo;
+< /dev/urandom tr -dc A-Z | head -c${1:-100};echo;
+< /dev/urandom tr -dc A-Z | head -c${1:-250};echo;
+< /dev/urandom tr -dc A-Z | head -c${1:-500};echo;
+< /dev/urandom tr -dc A-Z | head -c${1:-1000};echo;
+< /dev/urandom tr -dc A-Z | head -c${1:-10000};echo;
+
+

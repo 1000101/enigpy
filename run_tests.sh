51fe36ef5455f4a87f5652863df0dbfe4bcafc5a
Ján Hrnko <jan.hrnko@nexthop.sk>
2019-02-17 17:52:35 +0100

    Moar test

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 python3 -m pytest -s test_crypto.py
 
--- test_crypto.py
+++ test_crypto.py
@@ -83,13 +83,11 @@ def test_enigmaCase4():
 
     # Reflector B
     # Plugboard Empty
-    # Slowest rotor grund forced to pass Z
-    # Rotors with double step and single step mixed
 
     testNigma = crypto.Enigma(
         rotors = {
             1: components.Rotor("VI",0, 12),
-            2: components.Rotor("II",0, 0),
+            2: components.Rotor("VII",0, 0),
             3: components.Rotor("VIII",0, 0),
         },
         reflector = components.Reflector("B"),
@@ -104,51 +102,52 @@ def test_enigmaCase4():
     
     print('Done!')
 
-'''
-def test_enigmaCase3(self):
-    testNigma = Enigma(
+def test_enigmaCase5():
+    print('\nRunning test case 5')
+
+    # Reflector B
+    # Plugboard Empty
+    # Slowest rotor grund forced to pass Z
+    # Rotors with double step and single step mixed
+
+    testNigma = crypto.Enigma(
         rotors = {
-            1: Rotor("VIII",19-1, 0),
-            2: Rotor("II",7-1, 0),
-            3: Rotor("IV",12-1, 0),
+            1: components.Rotor("VI",0, 16),
+            2: components.Rotor("VII",0, 0),
+            3: components.Rotor("VIII",0, 0),
         },
-        reflector = Reflector("B"),
-        plugboard = Plugboard({"B":"D","C":"O","E":"I","G":"L","J":"S","K":"T","N":"V","P":"M","Q":"R","W":"Z"})
+        reflector = components.Reflector("B"),
+        plugboard = components.Plugboard({})
     )
-    self.assertEqual(testNigma.EDcrypt(""), "")
 
-def test_enigmaCase4(self):
-    testNigma = Enigma(
+    test_input = open('tests/testCases5.txt', 'r').read().split('\n\n')
+    
+    for line in test_input:
+        input_line, output_line = line.split('\n')
+        assert testNigma.EDcrypt(input_line) == output_line
+    
+    print('Done!')
+
+def test_enigmaCase6():
+    print('\nRunning test case 6')
+
+    # Reflector B
+    # Plugboard Empty
+
+    testNigma = crypto.Enigma(
         rotors = {
-            1: Rotor("VIII",19-1, 0),
-            2: Rotor("II",7-1, 0),
-            3: Rotor("IV",12-1, 0),
+            1: components.Rotor("VI",12, 16),
+            2: components.Rotor("VI",12, 16),
+            3: components.Rotor("II",12, 16),
         },
-        reflector = Reflector("B"),
-        plugboard = Plugboard({"B":"D","C":"O","E":"I","G":"L","J":"S","K":"T","N":"V","P":"M","Q":"R","W":"Z"})
+        reflector = components.Reflector("B"),
+        plugboard = components.Plugboard({})
     )
-    self.assertEqual(testNigma.EDcrypt(""), "")
-'''
 
-'''
-import unittest
-
-class Test_TestEnigma(unittest.TestCase):
-    def test_enigmaCase1(self):
-        testNigma = Enigma(
-            rotors = {
-                1: Rotor("VIII",19-1, 0),
-                2: Rotor("II",7-1, 0),
-                3: Rotor("IV",12-1, 0),
-            },
-            reflector = Reflector("B"),
-            plugboard = Plugboard({"B":"D","C":"O","E":"I","G":"L","J":"S","K":"T","N":"V","P":"M","Q":"R","W":"Z"})
-        )
-        self.assertEqual(testNigma.EDcrypt(""), "")
-
-    def test_enigmaCase2(self):
-        self.assertEqual(inc_dec.increment(3), "")
-
-if __name__ == '__main__':
-    unittest.main()
-'''
+    test_input = open('tests/testCases6.txt', 'r').read().split('\n\n')
+    
+    for line in test_input:
+        input_line, output_line = line.split('\n')
+        assert testNigma.EDcrypt(input_line) == output_line
+    
+    print('Done!')
